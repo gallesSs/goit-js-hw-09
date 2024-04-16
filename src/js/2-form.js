4258a1da2407d7313d8eb3e5933e916f98281b66
Artur <agavrik464@gmail.com>
2024-04-16 17:14:15 +0300

    updated js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,45 +1,35 @@
-const STORAGE_KEY = "feedback-form-state";
-const MAIL_KEY = "email-form-state";
-
 const form = document.querySelector(".feedback-form");
-const textarea = document.querySelector("textarea");
-const email = document.querySelector("input");
+const inputEmail = form.elements.email;
+const textarea = form.elements.message;
+const localStorageKey = "feedback-form-state";
 
-textarea.addEventListener("input", onTextareaInput);
-form.addEventListener("submit", handleSubmit);
-email.addEventListener("input", onEmailInput);
+updateFormInformation();
 
-populateTextarea();
-populateEmail();
+form.addEventListener("input", getFormInformation);
+function getFormInformation() {
+    const formObj = {
+        email: inputEmail.value.trim(),
+        message: textarea.value.trim()
+    };
+    localStorage.setItem(localStorageKey, JSON.stringify(formObj));
+};
 
-
-function onTextareaInput(event) {
-    const message = event.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
-}
-
-function onEmailInput(event) {
-    const mail = event.target.value;
-    localStorage.setItem(MAIL_KEY, mail);
-}
-
-function populateTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-    if (savedMessage) {
-        textarea.value = savedMessage;
+function updateFormInformation() {
+    const savedFormInfo = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+    inputEmail.value = savedFormInfo.email ?? "";
+    if (savedFormInfo) {
+        textarea.value = savedFormInfo.message ?? "";
     }
-}
+};
 
-function populateEmail() {
-    const savedMail = localStorage.getItem(MAIL_KEY);
-    if (savedMail) {
-        email.value = savedMail;
+form.addEventListener("submit", sentInformation);
+function sentInformation(evt) {
+    evt.preventDefault();
+    const savedFormInfo = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+
+    if (savedFormInfo.email && savedFormInfo.message) {
+        console.log(savedFormInfo);
     }
-}
-
-function handleSubmit(event) {
-    event.preventDefault();
-    event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-    localStorage.removeItem(MAIL_KEY)
-}
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+};
